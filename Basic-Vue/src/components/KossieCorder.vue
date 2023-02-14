b776c692d93533be158d68e994005a0015ec2364
jiae-kim <99962500+jiae-kim@users.noreply.github.com>
2023-02-15 00:02:47 +0900

    Create vue file with Vue Cli
    - Vue Cli로 뷰 설치 
    - Vue router
    - single file component

---
+++ Basic-Vue/src/components/KossieCorder.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{ name }}</p> <br>
+        <button @click="updateName">Change Name</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'Kossie Corder'
+        }
+    },
+
+    methods: {
+        updateName() {
+            this.name = 'Kossie Corder Updated';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ Basic-Vue/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>This is Home page</h1>    
+    <KossieCorder />
+  </div>
+</template>
+
+<script>
+import KossieCorder from '@/components/KossieCorder.vue';
+
+export default {
+  components: {
+    KossieCorder
+  },
+
+  data() {
+    return {
+      name: 'Jay Park'
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* scoped 작성시 해당 template에만 적용됨, 작성하지 않으면 전역으로 적용됨 */
+h1 {
+  color: blue;
+}
+</style>
