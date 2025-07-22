2a21e1578f9c64d282f3f15f7f65121c776fedbc
jiae-kim <kzioh950522@gmail.com>
2025-07-22 23:07:25 +0900

    ES6

---
+++ vue3.x/ch02/es6test/src/02-06.js
@@ -0,0 +1,7 @@
+function foodReport(name, age, ...favoriteFoods) {
+    console.log(name + ", " + age);
+    console.log(favoriteFoods);
+}
+
+foodReport("이몽룡", 20, "짜장면", "짬뽕", "탕수육");
+foodReport("홍길동", 16, "김밥");
