91dcb49c26e6145a3c3001ecbfd0dd06f319ceb7
jiae-kim <kzioh950522@gmail.com>
2025-08-24 23:02:29 +0900

    컴포넌트
    1. slot 기본 사용법
    2. named slot
    3. ScopedSlot

---
+++ vue3.x/ch08/slot-test/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <NoSlotTest />
+    <SlotTest/>
+    <NamedSlotTest />
+    <CheckBoxList />
+  </div>
+</template>
+
+<script>
+import NoSlotTest from './components/NoSlotTest.vue';
+import SlotTest from './components/SlotTest.vue';
+import NamedSlotTest from './components/NamedSlotTest.vue';
+import CheckBoxList from './components/CheckBoxList.vue';
+
+  export default {
+    name : "App",
+    components : {NoSlotTest, SlotTest, NamedSlotTest, CheckBoxList}
+  }
+</script>
+
+<style scoped>
+
+</style>
