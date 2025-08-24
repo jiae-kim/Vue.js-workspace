cb67220c2def1882b88ccd0f8b77cbb9a15ddfc1
jiae-kim <kzioh950522@gmail.com>
2025-08-24 23:50:41 +0900

    컴포넌트에서 v-model 디렉티브

---
+++ vue3.x/ch08/v-model-test/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- <InputName :name="searchName" @update:name="changeSearchName" /> -->
+    <InputName v-model:name="searchName" />
+    <h2>검색어 : {{ searchName }}</h2>
+  </div>
+</template>
+
+<script>
+import InputName from './components/InputName.vue';
+export default {
+  name : "App",
+  components : {InputName},
+  data() {
+    return {searchName : "John"}
+  },
+  methods : {
+    changeSearchName(name) {
+      this.searchName = name;
+    }
+  }
+}
+</script>
